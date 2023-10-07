bce6651f410966164f05d63a39e8788927e083db
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-10-07 19:59:25 +0300

    completed assignmnent

--- index.js
+++ index.js
@@ -20,4 +20,73 @@ const allWagesFor = function () {
 
     return payable
 }
+function createEmployeeRecord(array){
+    return{
+        firstName:array[0],
+        familyName:array[1],
+        title:array[2],
+        payPerHour:array[3],
+        timeInEvents:[], 
+        timeOutEvents:[],
+    }
+}
+function createEmployeeRecords(arr){
+    //or
+    
+    /*let newArr=[]
+    for(const items of arr){
+        newArr.push(items)
+    }
+    return newArr
+    */
+    let newArr=arr.map((array)=>{
+        return createEmployeeRecord(array)})
+  return newArr;
 
+}
+function  findEmployeeByFirstName(collection, firstNameString){
+     
+    let newCollection= collection.find((arrayo)=>{
+        return arrayo.firstName===firstNameString;
+    })
+    return newCollection;
+}
+function createTimeInEvent(dateStamp){
+    let newDateStamp=dateStamp.split(' ');
+    
+    let objecto = {
+    type: "TimeIn",
+    hour: parseInt(newDateStamp[1]),
+    date: newDateStamp[0]
+  };
+    this.timeInEvents.push(objecto);
+    return this;
+}
+//console.log(createTimeInEvent("2023-03-29 1728"))
+function createTimeOutEvent(dateStamp){
+  
+    let newDateStamp=dateStamp.split(' ')
+    let objectito = {
+    type: "TimeOut",
+    hour: parseInt(newDateStamp[1]),
+    date: newDateStamp[0]
+  };
+  this.timeOutEvents.push(objectito);
+ 
+  return this;
+};
+function hoursWorkedOnDate(date){
+    let timeInObj=this.timeInEvents.find(obj=>obj.date===date).hour
+    let timeOutObj=this.timeOutEvents.find(objo=>objo.date===date).hour
+   
+    return (timeOutObj-timeInObj)/100
+}
+  function wagesEarnedOnDate(date){
+   return hoursWorkedOnDate.call(this,date)*this.payPerHour;
+    //return hours()*this.payPerHour;
+  }
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, record) => {
+      return totalPayroll + allWagesFor.call(record)
+    }, 0)
+  }
